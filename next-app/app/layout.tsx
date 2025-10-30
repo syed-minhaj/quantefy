6a4d001d1dfe8117d5c3629e986968339432b2e1
syed-minhaj <minhajsulmuneeb@gmail.com>
2025-10-30 23:51:15 +0500

    feat: next top loader

--- next-app/app/layout.tsx
+++ next-app/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NextTopLoader from "nextjs-toploader";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NextTopLoader showSpinner={false} />
         {children}
       </body>
     </html>
