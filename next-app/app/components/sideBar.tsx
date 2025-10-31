2f8923c27fd420290c42728d7a5fcc4db17178d7
syed-minhaj <minhajsulmuneeb@gmail.com>
2025-10-31 23:27:04 +0500

    feat: update sidebar

--- next-app/app/components/sideBar.tsx
+++ next-app/app/components/sideBar.tsx
@@ -15,28 +15,28 @@ const IconComponent = ({page}:{page : string}) => {
     else return <div className="h-5 w-5"/>
 }
 
-export default function SideBar() {
+export default function SideBar({store}:{store:string}) {
 
     const [open, setOpen] = useState(true);
     const pathname = usePathname()
     
 
     return (
-        <div className={`${open ? "w-60" : "w-17.5"} relative h-screen bg-sidebar flex flex-col  transition-all duration-500 ease-in-out sideBar-parent  `}>
+        <div className={`${open ? "w-60" : "w-17.5"} hidden sticky top-0 h-screen bg-sidebar sm:flex flex-col  transition-all duration-300 ease-in-out sideBar-parent  `}>
             <div className="h-16 flex flex-row pt-4 pl-6">
-                <h1 className={`font-bold ${open ? "w-32" : "w-0"} overflow-hidden transition-all duration-500 ease-in-out logo `}>Quantefy</h1>
+                <h1 className={`font-bold ${open ? "w-32" : "w-0"} overflow-hidden transition-all duration-300 ease-in-out logo `}>Quantefy</h1>
                 <MoveLeft className={` absolute right-5.75 p-1 text-black/85 border border-black/20 rounded  ${open ? "" : "rotate-180 "} `} 
                 onClick={() => setOpen(!open)} />
             </div>
             <div className="px-4 flex flex-col gap-1 flex-1 group sideBar-child ">
                 {Pages.map((page, index) => (
-                    <Link href={`/app/${page}`} key={index} 
-                        className={` ${open ? "w-52 " : "w-9.5"} rounded-[0.5rem] border flex flex-row items-center  transition-all duration-500 ease-in-out group-hover:w-52
-                        ${pathname.split("/app/").at(-1) == page ? "bg-prePrimary  border-black/20" : "border-transparent"} 
+                    <Link href={`/app/store/${store}/${page}`} key={index} 
+                        className={` ${open ? "w-52 " : "w-9.5"} rounded-[0.5rem] border flex flex-row items-center  transition-all duration-300 ease-in-out group-hover:w-52
+                        ${pathname.split(`/app/store/${store}/`).at(-1) == page ? "bg-prePrimary  border-black/20" : "border-transparent"} 
                         p-2 px-2 leading-none font-medium text-black/85 `} >
                         <IconComponent page={page} />
-                        <span className={`${open ? "w-24" : "w-0 "} overflow-hidden whitespace-nowrap  transition-all duration-500 ease-in-out group-hover:w-24 `}>
-                            &nbsp;{'  ' + page.charAt(0).toUpperCase() + page.slice(1)}
+                        <span className={`${open ? "w-24" : "w-0 "} overflow-hidden whitespace-nowrap  transition-all duration-300 ease-in-out group-hover:w-24 `}>
+                            &nbsp;{page.charAt(0).toUpperCase() + page.slice(1)}
                         </span>
                     </Link>
                 ))}
