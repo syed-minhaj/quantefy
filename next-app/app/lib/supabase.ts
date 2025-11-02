da8725bc25ea85ad50aa983a7c2b9d67893aa9b8
syed-minhaj <minhajsulmuneeb@gmail.com>
2025-11-02 14:57:43 +0500

    feat: supabase setup

---
+++ next-app/app/lib/supabase.ts
@@ -0,0 +1,16 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+    process.env.SUPABASE_URL ?? " ",
+    process.env.SUPABASE_ANON_KEY ?? " ",
+    {
+      auth: {
+        persistSession: false,
+      },
+      global: {
+        headers: {
+          'x-api-key': process.env.SUPABASE_BUCKET_API_KEY ?? " ",
+        },
+      },
+    },
+);
