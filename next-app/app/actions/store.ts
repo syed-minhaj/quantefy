c41a221c9cd529e04ed5ceb9e23e64f2f38f64d4
syed-minhaj <minhajsulmuneeb@gmail.com>
2025-11-03 01:14:43 +0500

    feat: add item server action

--- next-app/app/actions/store.ts
+++ next-app/app/actions/store.ts
@@ -3,9 +3,12 @@ import { auth } from "@/app/lib/auth";
 import { headers } from "next/headers";
 import { supabase } from "../lib/supabase";
 import { db } from "../lib/drizzle";
-import { store } from "@/db/schema";
+import { item, store } from "@/db/schema";
 import { eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
+
+
+const supabaseUrl = process.env.SUPABASE_URL + "/storage/v1/object/public";
 
 export const createStore = async (name:string , description:string , logo:File | null) => {
     const session = await auth.api.getSession({headers: await headers()});
@@ -13,7 +16,6 @@ export const createStore = async (name:string , description:string , logo:File |
     if(name == "") return {err : "Name is required"};
     
     try{
-        const supabaseUrl = process.env.SUPABASE_URL + "/storage/v1/object/public";
     
         const storeID = await db.insert(store).values({
             name,
@@ -36,6 +38,44 @@ export const createStore = async (name:string , description:string , logo:File |
                     .where(eq(store.id , storeID[0].insertedID));
             
         revalidatePath("/app");
+        
+    }catch(e){
+        console.log(e)
+        return {err : "Failed to create store"}
+    }
+    return {err : null}
+
+}
+
+export const addItem = async (name:string , price:number , cost:number , quantity:number , picture:File , storeID:string) => {
+    const session = await auth.api.getSession({headers: await headers()});
+    if (!session) return {err : "Sign in to create store"};
+    if(name == "") return {err : "Name is required"};
+    
+    try{
+    
+        const itemID = await db.insert(item).values({
+            name,
+            sale_price : price,
+            cost_price : cost,
+            quantity,
+            store_id : storeID,
+            picture : "",
+        }).returning({insertedID : item.id});
+    
+        const {data , error} = await supabase.storage
+            .from("quantefy")
+            .upload(`item/${itemID[0].insertedID}/picture.png` , picture ,{
+                                cacheControl: '3600', 
+                                contentType: 'image/png', 
+                                upsert: false, 
+                            });
+
+        if(error) console.log(error)
+        if(!error) await db.update(item).set({picture: `${supabaseUrl}/quantefy/item/${itemID[0].insertedID}/picture.png`})
+                    .where(eq(item.id , itemID[0].insertedID));
+            
+        revalidatePath(`/app/store/${storeID}/inventory`);
         
     }catch(e){
         console.log(e)
