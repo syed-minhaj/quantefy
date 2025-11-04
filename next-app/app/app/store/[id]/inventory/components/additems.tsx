1dff06730f8b42bd77c1d16092fd0b1a238df0c3
syed-minhaj <minhajsulmuneeb@gmail.com>
2025-11-04 21:03:51 +0500

    feat: update add item , close outside form .

--- next-app/app/app/store/[id]/inventory/components/additems.tsx
+++ next-app/app/app/store/[id]/inventory/components/additems.tsx
@@ -1,9 +1,8 @@
 "use client"
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { Button } from "@/app/components/ui/button"
 import { X } from "lucide-react";
 import { Input } from "@/app/components/ui/input";
-import { Textarea } from "@/app/components/ui/textarea";
 import { toast } from "sonner";
 import { addItem } from "@/app/actions/store";
 
@@ -16,6 +15,7 @@ export default function AddItem({storeID}:{storeID:string}) {
     const [quantity, setQuantity] = useState(0);
     const [picture, setPicture] = useState<File | null>(null);
     const [adding, setAdding] = useState(false);
+    const formRef = useRef<HTMLDivElement>(null);
 
 
     function add(){
@@ -24,6 +24,9 @@ export default function AddItem({storeID}:{storeID:string}) {
             return;
         };
         setAdding(true);
+        toast.loading("Adding item...",{
+            id: "addItem",
+        });
         addItem(name , price , cost , quantity , picture , storeID).then((res) => {
             if(res.err) toast.error(res.err);
             else {
@@ -35,25 +38,42 @@ export default function AddItem({storeID}:{storeID:string}) {
                 setQuantity(0);
                 setPicture(null);
             }
+            toast.dismiss("addItem");
             setAdding(false);
         })
     }
+
+    useEffect(() => {
+
+        const handleClickOutside = async (event:MouseEvent) => {
+            if (!formRef.current?.contains(event.target  as Node) && event.target != document.getElementById("addItem")) {
+                setOpen(false);
+            }
+        };
+
+        document.addEventListener('click', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, [])
+
     return (
         <>
-            <Button className="rounded-r1 ml-auto" variant={"default"} onClick={() => setOpen(!open)}>
+            <Button className="rounded-r1 ml-auto" variant={"default"} onClick={() => setOpen(!open)} id="addItem">
                     Add Store
             </Button>
             <div className={`${open ? "fixed" : "hidden"} top-0 left-0 w-screen h-screen bg-black/50 flex items-center justify-center z-100`}  >
-                <div className={` w-120 h-5/6 rounded-r1 bg-bg1 relative p-4 flex flex-col gap-4 z-110`} >
+                <div className={` w-120 h-5/6 rounded-r1 bg-bg1 relative p-4 flex flex-col gap-4 z-110`} ref={formRef} >
                     <X className="m-4 absolute top-0 right-0 opacity-50 hover:opacity-100 " size={16} onClick={() => setOpen(!open)}/>
                     <h2 className="font-medium ml-1 ">Add Item</h2>
                     <div className="flex flex-col gap-1  text-normal">
                         <Input placeholder="Name" type="text" value={name} className="bg-bg2 rounded-r1 disabled:opacity-50 " disabled={adding} required
                             onChange={(e) => setName(e.target.value)}/>
                         <div className="flex flex-row gap-2">
-                            <Input placeholder="Price" type="number" value={price}  className="bg-bg2 rounded-r1 disabled:opacity-50 " disabled={adding} required
+                            <Input placeholder="Price" type="number" value={price ?? ""}  className="bg-bg2 rounded-r1 disabled:opacity-50 " disabled={adding} required
                                 onChange={(e) => setPrice(Number(e.target.value))}/>
-                            <Input placeholder="Cost" type="number" value={cost}  className="bg-bg2 rounded-r1 disabled:opacity-50 " disabled={adding} required
+                            <Input placeholder="Cost" type="number" value={cost ?? ""}  className="bg-bg2 rounded-r1 disabled:opacity-50 " disabled={adding} required
                                 onChange={(e) => setCost(Number(e.target.value))}/>
                         </div>
                         <span className={`ml-1 text-sm font-medium ${adding ? "opacity-50" : ""}`} >Qunatity</span>
