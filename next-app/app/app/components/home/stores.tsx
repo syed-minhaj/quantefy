6f7b32e11c127aabbb5a4208aa45728bfa683ba4
syed-minhaj <minhajsulmuneeb@gmail.com>
2025-11-28 00:16:16 +0500

    feat: update hover effect for item and store

--- next-app/app/app/components/home/stores.tsx
+++ next-app/app/app/components/home/stores.tsx
@@ -10,7 +10,7 @@ export default function Stores({stores}:{stores:store[]}) {
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
                 {stores.map((store : any) => (
                     <Link  href={`/app/store/${store.id}/dashboard`}
-                        className="bg-bg2 border hover:border-black/20 rounded-r1 p-4 flex flex-col gap-4 h-40 " key={store.id}>
+                        className="bg-bg2 border hover:border-black/20 dark:hover:border-white/40 rounded-r1 p-4 flex flex-col gap-4 h-40 " key={store.id}>
                         <div className="flex flex-row items-center gap-2">
                             {store.logo &&
                                 <Image src={store.logo} alt="logo" width={32} height={32} className="rounded-r1 bg-bg2 h-8 w-8 object-contain border border-input" />
--- next-app/app/app/store/[id]/inventory/components/item.tsx
+++ next-app/app/app/store/[id]/inventory/components/item.tsx
@@ -8,7 +8,7 @@ export default function Item({item}:{item:item}) {
     const {updateHash} = useHash("")
 
     return (
-        <div className="flex flex-col gap-1 bg-bg2 rounded-r1 border border-input w-full sm:w-64 p-2 hover:border-black/20 " 
+        <div className="flex flex-col gap-1 bg-bg2 rounded-r1 border border-input w-full sm:w-64 p-2 hover:border-black/20 dark:hover:border-white/40" 
         key={item.id} onClick={() => updateHash(item.id)} >
             <Image src={item.picture} alt="logo" width={256} height={192} className="rounded-r1 bg-bg1 h-48 w-full object-contain border border-input" />
             
