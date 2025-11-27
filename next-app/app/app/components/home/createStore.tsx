7a6ab3cc0d7e67d268570f71b704e0d09dcd2df4
syed-minhaj <minhajsulmuneeb@gmail.com>
2025-11-28 00:11:52 +0500

    feat: updating dark mode background color

--- next-app/app/app/components/home/createStore.tsx
+++ next-app/app/app/components/home/createStore.tsx
@@ -48,7 +48,7 @@ export default function CreateStore({open , setOpen}:{open :Boolean , setOpen:Re
 
 
     return(
-        <div className={`${open ? "fixed" : "hidden"} top-0 left-0 w-screen h-screen bg-black/50  flex items-center justify-center z-100`}  >
+        <div className={`${open ? "fixed" : "hidden"} top-0 left-0 w-screen h-screen bg-black/50 dark:bg-gray-800/50 flex items-center justify-center z-100`}  >
             <div className={` w-120 h-5/6 rounded-r1 bg-bg1 relative p-4 flex flex-col gap-4 z-110`} ref={formRef} >
                 <X className="m-4 absolute top-0 right-0 opacity-50 hover:opacity-100 " size={16} onClick={() => setOpen(!open)}/>
                 <h2 className="font-medium ml-1 ">Add Store</h2>
